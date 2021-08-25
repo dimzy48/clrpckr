9ac0197c861d48ae722b2f84bca3628590e9a1dc
chana <chanaseptiari@gmail.com>
2021-08-25 08:29:47 +0700

    Pick A Color

--- picker.py
+++ picker.py
@@ -1,30 +1,24 @@
-import pynput
-from pynput import keyboard
-from pynput import mouse
-from PIL import Image, ImageGrab
+from pynput import keyboard, mouse
+from PIL import ImageGrab
 
 def getHex(rgb):
     return '%02X%02X%02X'%rgb
 
-
-def checkColor(x,y):
+def getRGB(x,y):
     bbox = (x,y,x+1,y+1)
     im = ImageGrab.grab(bbox=bbox)
     rgbim = im.convert('RGB')
     r,g,b = rgbim.getpixel((0,0))
-    print(f'COLOR: rgb{(r,g,b)} | HEX #{getHex((r,g,b))}')
-    
-   
+    return (r,g,b)
+
 def onClick(x,y, button, pressed):
 	if pressed and button == mouse.Button.left:
-		checkColor(x,y)
+		print(f'RGB = {getRGB(x,y)}\t| HEX=#{getHex(getRGB(x,y))}')
 
 def onRel(key):
 	if key == keyboard.Key.esc:
 		mlstnr.stop()
 		return False
-
-
 
 if __name__ == '__main__':
     with keyboard.Listener(on_release = onRel) as klstnr:
